60e392e3abb98ab43876db07c54ee569d889f1b0
sristi24 <sristi.gupta92@gmail.com>
2021-08-15 17:53:54 +0530

    string service call

--- com/practiceGit/sristi/service/Service.java
+++ com/practiceGit/sristi/service/Service.java
@@ -1,15 +1,15 @@
 
 
 public class Service {
-
-
+    
     public static int serviceCall () {
 
         return 0;
     }
 
+    public static String serviceCall () {
 
-
-
+        return null;
+    }
 
 }
