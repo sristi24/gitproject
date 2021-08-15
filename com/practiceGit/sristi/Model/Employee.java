01eae7d0aca7861cdf0a08bb782c9b1543b99300
sristi24 <sristi.gupta92@gmail.com>
2021-08-15 20:57:51 +0530

    employee model with pojo

--- com/practiceGit/sristi/Model/Employee.java
+++ com/practiceGit/sristi/Model/Employee.java
@@ -3,8 +3,37 @@
 
 public class Employee {
 
+
+
     String name;
     int employeeId;
     String mobileNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+
+
 
 }
